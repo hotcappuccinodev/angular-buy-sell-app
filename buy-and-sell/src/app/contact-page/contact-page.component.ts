3b5c7f6527d8f520c3ea3315d9933b7d366d268d
ruikishimoto <ruikishimoto99@gmail.com>
2022-08-03 20:04:51 -0400

    added the Contact Seller page

--- buy-and-sell/src/app/contact-page/contact-page.component.ts
+++ buy-and-sell/src/app/contact-page/contact-page.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute,Router } from '@angular/router';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.css']
 })
 export class ContactPageComponent implements OnInit {
+  email: string='';
+  message: string='';
+  listing: Listing;
+  constructor(private route:ActivatedRoute,
+    private router: Router,
+    ) { 
+    
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    const id =this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id)!;
+    this.message = `Hi, I'm interested in your ${this.listing.name.toLowerCase()}`
+
+  
+    
+  }
+
+  sendMessage(): void{
+    alert('Your message has been sent!');
+    this.router.navigateByUrl('/listings');
+  }
 }
