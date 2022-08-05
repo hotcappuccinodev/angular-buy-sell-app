1ad0fdd33ae764e0c42eda879f16f1574c28313a
ruikishimoto <ruikishimoto99@gmail.com>
2022-08-04 20:19:59 -0400

    added the New Listings page

--- buy-and-sell/src/app/new-listing-page/new-listing-page.component.ts
+++ buy-and-sell/src/app/new-listing-page/new-listing-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-new-listing-page',
   templateUrl: './new-listing-page.component.html',
   styleUrls: ['./new-listing-page.component.css']
 })
 export class NewListingPageComponent implements OnInit {
+  name : string = '';
+  description: string = '';
+  price: string='';
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
+
+  }
+
+  onSubmit(): void {
+    alert('Creating a new listing...');
+    this.router.navigateByUrl('/my-listings');
   }
 
 }
