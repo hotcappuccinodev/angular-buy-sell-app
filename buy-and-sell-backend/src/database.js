5eaa56a3c441aa3d71003e752ca0d10216853cd5
ruikishimoto <ruikishimoto99@gmail.com>
2022-08-24 18:56:04 -0400

    added the views endpoint

--- buy-and-sell-backend/src/database.js
+++ buy-and-sell-backend/src/database.js
@@ -3,19 +3,18 @@ import mysql from 'mysql';
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'hapi-server',
-    password: '1234',
+    password: 'abc123!',
     database: 'buy-and-sell',
-
-})
+});
 
 export const db = {
     connect: () => connection.connect(),
-    query: (queryString, escapedValues) => 
-        new PromiseRejectionEvent((resolve, reject) => {
-            connection.query(queryString,escapedValues, (error,results,fields) => {
-              if (error) reject(error);
-              resolve({results, fields});  
-            } )   
+    query: (queryString, escapedValues) =>
+        new Promise((resolve, reject) => {
+            connection.query(queryString, escapedValues, (error, results, fields) => {
+                if (error) reject(error);
+                resolve({ results, fields });
+            })
         }),
-        end: ()=> connection.end(),
+    end: () => connection.end(),
 }
--- buy-and-sell-backend/src/routes/index.js
+++ buy-and-sell-backend/src/routes/index.js
@@ -1,6 +1,8 @@
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListing";
+import { addViewToListingRoute } from "./addViewToListing";
 export default [
     getAllListingsRoute,
     getListingRoute,
+    addViewToListingRoute,
 ];
