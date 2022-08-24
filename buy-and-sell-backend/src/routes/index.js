cc0c0cead9fce8f49b862037fa042068d1fb814e
ruikishimoto <ruikishimoto99@gmail.com>
2022-08-24 19:04:14 -0400

    added the user listing endpoint

--- buy-and-sell-backend/src/routes/index.js
+++ buy-and-sell-backend/src/routes/index.js
@@ -1,8 +1,10 @@
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListing";
 import { addViewToListingRoute } from "./addViewToListing";
+import { getUserListingsRoute } from "./getUserListings";
 export default [
     getAllListingsRoute,
     getListingRoute,
     addViewToListingRoute,
+    getUserListingsRoute,
 ];
