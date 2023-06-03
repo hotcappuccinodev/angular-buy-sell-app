4c5bb2b89c8071a6264da70c680759c6cc522e14
ruikishimoto <ruikishimoto99@gmail.com>
2023-06-02 20:56:25 -0400

    added data to the listings page

--- buy-and-sell/src/app/listings-page/listings-page.component.ts
+++ buy-and-sell/src/app/listings-page/listings-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Listing} from '../types';
+import { fakeListings } from '../fake-data';
 @Component({
   selector: 'app-listings-page',
   templateUrl: './listings-page.component.html',
   styleUrls: ['./listings-page.component.css']
 })
 export class ListingsPageComponent {
+  listings: Listing[] = [];
 
+  ngOnInit(): void{
+    this.listings=fakeListings;
+  }
 }
---
+++ buy-and-sell/src/app/types.ts
@@ -0,0 +1,9 @@
+export interface Listing{
+  id: string,
+  name: string,
+  description: string,
+  price: number,
+
+
+
+}
